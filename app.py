5020e63ccd9efddc54b6a39b6b4fa31f881f96c2
Sagar <tikaresagar9@gmail.com>
2025-10-09 16:45:51 +0530

    initial second commit-CICD project

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Sagar's CI/CD Project!"
+    return "Hello from Sagar's CI Project!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
